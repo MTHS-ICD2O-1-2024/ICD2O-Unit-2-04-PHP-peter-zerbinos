c359e61feba264f44340daad811c9d98b107365f
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-04-28 02:19:10 +0000

    added code

---
+++ answer.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!-- ICS2O-Unit2-04-PHP -->
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Area of Triangle, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Peter Zerbinos" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.light_blue-orange.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-icon.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Area of Triangle, in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Area of Triangle, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/area_of_trinagle.png" alt="triangle" />
+      </div>
+      <div class="page-content-php">
+        <div id="user-info">
+          The area is: 8 cm². </div>
+        </br>
+      </div>
+      <a href="./index.php">Return ...</a>
+  </div>
+  </div>
+  </main>
+  </div>
+</body>
+
+</html>
